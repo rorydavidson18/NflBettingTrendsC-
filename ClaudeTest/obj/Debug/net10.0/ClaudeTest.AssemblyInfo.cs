0d46827c69781c2a120a448e2ed1be1d702a7a3e
Rory Davidson <rorydavidson18@gmail.com>
2026-08-03 12:04:58 -0400

    fixed the index page

--- ClaudeTest/obj/Debug/net10.0/ClaudeTest.AssemblyInfo.cs
+++ ClaudeTest/obj/Debug/net10.0/ClaudeTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClaudeTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+290d77fdb6e954ad769f28673bb9e533268f27ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2eef493ae5fb0d2b13d82da020e3b24898d9dbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClaudeTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClaudeTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
